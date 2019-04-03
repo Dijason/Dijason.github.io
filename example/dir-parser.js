18bc712836f61c7bb692cf3241cd0257f0ac11f8
daizch <daizech@gmail.com>
2019-04-03 11:18:43 +0800

    add dir-parser

---
+++ example/dir-parser.js
@@ -0,0 +1,58 @@
+var fs = require('fs')
+var text = fs.readFileSync('./doc').toString()
+// console.log(JSON.stringify(text))
+
+function parseLine(line) {
+  var reg = /^(\s*)([^\s]+)$/
+  var $$ = reg.exec(line)
+  if (!$$[1]) {
+    return {
+      pad: 0,
+      value: line
+    }
+  } else {
+    return {
+      pad: $$[1].length,
+      value: $$[2]
+    }
+  }
+}
+
+function dirParser(text) {
+  var lines = text.split('\n')
+  var stack = []
+  var result = []
+
+  var currentIndent = 0
+  var indentTokenCaches = {}
+  lines.forEach(line => {
+    var currentLine = parseLine(line)
+    if (currentLine.pad === currentIndent) {
+      result.push(currentLine)
+    } else if (currentLine.pad > currentIndent) {
+      currentIndent = currentLine.pad
+      stack.push(currentLine)
+    } else if (indentTokenCaches[currentLine.pad]) {
+      let token = stack.pop()
+      while (token && token.pad > currentLine.pad) {
+        currentIndent = token.pad
+        result.push(token)
+        token = stack.pop()
+      }
+
+      if (token) {
+        stack.push(token)
+        stack.push(currentLine)
+      } else {
+        result.push(currentLine)
+      }
+    } else {
+      throw new Error(`error indent: ${line}`)
+    }
+    indentTokenCaches[currentLine.pad] = true
+  })
+
+  return result
+}
+
+console.log(JSON.stringify(dirParser(text)))
